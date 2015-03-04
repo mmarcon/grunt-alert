856186161b615edf11100fa34aa0f7f1e8faa5f0
Massimiliano Marcon <me@marcon.me>
2015-03-04 08:07:16 +0100

    drafted hipchat module

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,6 +15,7 @@ module.exports = function(grunt) {
         jshint: {
             all: [
                 'Gruntfile.js',
+                'lib/*.js',
                 'tasks/*.js',
                 'tests/*.js'
             ],
